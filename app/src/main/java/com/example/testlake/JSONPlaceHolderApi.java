c4ba7f8168a757b1d45bbcb3ef638b8b31989bdd
Nikita-Freedom <44145716+Nikita-Freedom@users.noreply.github.com>
2020-02-10 01:12:27 +0200

    DocumentProvider. Retrofit

---
+++ app/src/main/java/com/example/testlake/JSONPlaceHolderApi.java
@@ -0,0 +1,4 @@
+package com.example.testlake;
+
+public class JSONPlaceHolderApi {
+}
---
+++ app/src/main/java/com/example/testlake/NetworkService.java
@@ -0,0 +1,29 @@
+package com.example.testlake;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkService {
+    private static NetworkService mInstance;
+    private static final String BASE_URL = "http://172.20.10.3:19918";
+    private Retrofit mRetrofit;
+
+    private NetworkService() {
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+    GetApiInterface mGetApiInterface = mRetrofit.create(GetApiInterface.class);
+
+    public static NetworkService getInstance() {
+        if (mInstance == null) {
+            mInstance = new NetworkService();
+        }
+        return mInstance;
+    }
+    public JSONPlaceHolderApi getJSONApi() {
+        return mRetrofit.create(JSONPlaceHolderApi.class);
+    }
+
+}
