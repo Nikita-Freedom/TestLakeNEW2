e4b3c7e9afb8ca71b8d59f8051a9a3374ab519ab
Nikita <milena1990_bk.ru@icloud.com>
2020-02-10 15:32:45 +0200

    commit

--- app/src/main/java/com/example/testlake/storage/FileRepository.java
+++ app/src/main/java/com/example/testlake/storage/FileRepository.java
@@ -1,17 +1,26 @@
 package com.example.testlake.storage;
 
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class FileRepository {
 
     private static FileRepository INSTANCE;
     // TODO: задать базовый URL, используется во всех методах
-    private String BASE_URL = "";
+    private String BASE_URL = "localhost";
 
     public static FileRepository getInstance() {
         if (INSTANCE == null) {
@@ -29,12 +38,24 @@ public class FileRepository {
         if (!dir.isDirectory()) {
             throw new IllegalArgumentException(dir.getPath() + "not a directory");
         }
+        Document document = null;
 
-        // Работаем с путём
-        String path = dir.getPath();
+        try {
+            document = Jsoup.connect(BASE_URL).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        return new ArrayList<>();
-    }
+        if (document != null)
+            getFilesList(FileEntity.ROOT);
+        else
+        return null;
+
+
+    // Работаем с путём
+    String path = dir.getPath();
+    return new ArrayList<>();
+}
 
     // TODO: можно использовать HttpUrlConnection,
     //       чтобы открыть файл на сервере и передать полученный InputStream в провайдер
